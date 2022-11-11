6dc2652d4774ffbf52ad101cfa005b3e4d7569a3
Belskaya20 <110544821+Belskaya20@users.noreply.github.com>
2022-11-11 15:53:15 +0300

    Экзамен-проект

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ document.addEventListener("click", (event) => {
 })
 //нумерация карточек
 function makeCounter() {
-  let i  = 1;
-  return function() {
+  let i = 1;
+  return function () {
     return i++;
   };
 }
@@ -24,40 +24,46 @@ let counter = makeCounter();
 function random() {
   const words = [
     { name: 'Cat', translate: 'Кот', example: 'Сat with big green eyes' },
-    { name: 'Milk', translate: 'Молоко', example: 'I love chocolate milk' },
     { name: 'Book', translate: 'Книга', example: 'Read book carefully' },
-    { name: 'Pencil', translate: 'Карандаш', example: 'Write in a notebook with a pencil' },
     { name: 'Coffee', translate: 'Кофе', example: 'Cup of coffee in the morning' },
-    { name: 'Small', translate: 'Маленький', example: 'Little white puppy' },
     { name: 'Orange', translate: 'Оранжевый', example: 'Orange flower on the table' },
     { name: 'Children', translate: 'Дети', example: 'Children on the playground' },
-    { name: 'Husband', translate: 'Муж', example: 'She has a tall husband' }
   ];
   const randWord = Math.floor(Math.random());
   return words[randWord];
 }
 
 //С помощью стрелок можно листать карточки вперед-назад
-//Добавила в верстку для div класс itcss
-document.addEventListener('DOMContentLoaded', () => {
-  const slider = new ItcSimpleSlider('.itcss');
-  //обработчик при нажатии на кнопку back
-  document.querySelector('#back').onclick = () => {
-    slider.back();
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slider = document.getElementsByClassName('slider');
+  if (n > slider.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slider.length }
+  for (i = 0; i < slider.length; i++) {
+    slider[i].style.display = "none";
   }
-  //обработчик при нажатии на кнопку next
-  document.querySelector('#next').onclick = () => {
-    slider.next();
-  }
+  slider[slideIndex - 1].style.display = "block";
   //блокировка стрелок
   back.classList.toggle('disabled', slider === 0);
   next.classList.toggle('disabled', slider === --slider.length);
-});
+}
 
 
 
 //Режим проверки знаний 
-const examCards = document.querySelector('#exam-cards');
 buttonExam.addEventListener("click", (event) => {
-  //не знаю как осуществить переход((
+    document.getElementById("exam-mode").style.display = "block";
+    document.getElementById("study-mode").style.display = "none";
 })
