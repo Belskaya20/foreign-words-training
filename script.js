0a73fc6a6b6b415947ac069df47acca8af289039
Belskaya20 <110544821+Belskaya20@users.noreply.github.com>
2022-11-13 14:37:32 +0300

    Экзамен-проект

--- script.js
+++ script.js
@@ -1,13 +1,79 @@
 const flipCard = document.querySelector('.flip-card');
-const cardFront = document.querySelector('#card-front');
-const cardBack = document.querySelector('#card-back');
+const cardFront = document.querySelector('#card-front h1');
+const cardBack = document.querySelector('#card-back h1');
+const cardExample = document.querySelector('#card-back span');
 const buttonShuffle = document.querySelector('#shuffle-words');
 const buttonExam = document.querySelector('#exam');
+const buttonBack = document.querySelector('#back');
+const buttonNext = document.querySelector('#next');
+const studyBlock = document.querySelector('.study-cards');
+const studyMode = document.querySelector('#study-mode');
+const examMode = document.querySelector('#exam-mode');
 
+
+
+//Карточки на одной стороне (#card-front) содержат иностранное слово, а на другой (#card-back) — его перевод и пример использования
+function random() {
+  //записала эту штуку отдельными массивами
+  const words = {
+    cat: 'Cat',
+    book: 'Book',
+    coffee: 'Coffee',
+    orange: 'Orange',
+    children: 'Children',
+    people: 'People',
+    animal: 'Animal',
+    grass: 'Grass',
+    mirror: 'Mirror',
+    strawberry: 'Strawberry'
+  };
+
+  const translate = {
+    cat: 'Кот',
+    book: 'Книга',
+    coffee: 'Кофе',
+    orange: 'Оранжевый',
+    children: 'Дети',
+    people: 'Люди',
+    animal: 'Животное',
+    grass: 'Трава',
+    mirror: 'Зеркало',
+    strawberry: 'Клубника'
+  };
+
+  const example = {
+    cat: 'Сat with big green eyes',
+    book: 'Read book carefully',
+    coffee: 'Cup of coffee in the morning',
+    orange: 'Orange flower on the table',
+    children: 'Children on the playground',
+    people: 'These are people from England',
+    animal: 'There are a lot of animals in this zoo',
+    grass: 'They are sitting on the grass and having picnic',
+    mirror: 'Look in the mirror!',
+    strawberry: 'My favorite berries are strawberries'
+  };
+
+
+
+  const randWord = Math.floor(Math.random() * (Object.keys(words).length));
+
+  const wordShow = Object.keys(words);
+  cardFront.innerHTML = (wordShow[random]);
+  const translateShow = Object.values(translate);//при перевороте карточки пишет Undefined
+  cardBack.innerHTML = (translateShow[random]);
+  const exampleShow = Object.values(example);//при перевороте карточки пишет Undefined
+  cardExample.innerHTML = (exampleShow[random]);
+
+
+  return wordShow[randWord];
+}
+
+cardFront.innerHTML = random();
 
 
 //Поворот карточки с добавлением класса active
-document.addEventListener("click", (event) => {
+flipCard.addEventListener("click", (event) => {
   flipCard.classList.add('active');
 })
 //нумерация карточек
@@ -19,22 +85,8 @@ function makeCounter() {
 }
 let counter = makeCounter();
 
-
-//Карточки на одной стороне (#card-front) содержат иностранное слово, а на другой (#card-back) — его перевод и пример использования
-function random() {
-  const words = [
-    { name: 'Cat', translate: 'Кот', example: 'Сat with big green eyes' },
-    { name: 'Book', translate: 'Книга', example: 'Read book carefully' },
-    { name: 'Coffee', translate: 'Кофе', example: 'Cup of coffee in the morning' },
-    { name: 'Orange', translate: 'Оранжевый', example: 'Orange flower on the table' },
-    { name: 'Children', translate: 'Дети', example: 'Children on the playground' },
-  ];
-  const randWord = Math.floor(Math.random());
-  return words[randWord];
-}
-
-//С помощью стрелок можно листать карточки вперед-назад
-
+//Перелистывание карточек вперед-назад
+//Наверное допустила ошибку, не работает
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -56,14 +108,14 @@ function showSlides(n) {
   }
   slider[slideIndex - 1].style.display = "block";
   //блокировка стрелок
-  back.classList.toggle('disabled', slider === 0);
-  next.classList.toggle('disabled', slider === --slider.length);
+  buttonBack.classList.toggle('disabled', slider === 0);
+  buttonNext.classList.toggle('disabled', slider === --slider.length);
 }
 
-
-
-//Режим проверки знаний 
+//Переключение между режимами
+//Добавила класс hidden, работает
 buttonExam.addEventListener("click", (event) => {
-    document.getElementById("exam-mode").style.display = "block";
-    document.getElementById("study-mode").style.display = "none";
+  studyBlock.classList.add("hidden");
+  studyMode.classList.add("hidden");
+  examMode.classList.remove("hidden");
 })
